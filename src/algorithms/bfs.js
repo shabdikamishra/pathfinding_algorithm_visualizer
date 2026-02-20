485ff8eed094051843a2dd1153274a32345264bb
shabdikamishra <shabdikamishra30@gmail.com>
2026-02-20 13:51:03 +0530

    Fix boundary checks in getNeighbors function

--- src/algorithms/bfs.js
+++ src/algorithms/bfs.js
@@ -32,8 +32,8 @@ function getNeighbors(grid, node) {
 const neighbors = [];
 const { row, col } = node;
 if (row > 0) neighbors.push(grid[row - 1][col]);
-if (row <= grid.length) neighbors.push(grid[row + 1][col]);
+if (row < grid.length -1) neighbors.push(grid[row + 1][col]);
 if (col > 0) neighbors.push(grid[row][col - 1]);
-if (col <= grid[0].length) neighbors.push(grid[row][col + 1]);
+if (col < grid[0].length -1) neighbors.push(grid[row][col + 1]);
 return neighbors;
 }
